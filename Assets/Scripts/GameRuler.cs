c7c15498f6a252a28f49e6004c2d8de6c24bd5f5
ReWriteGame <mr.ekstrimal1432@gmail.com>
2021-09-08 00:24:15 +0300

    init commit

---
+++ Assets/Scripts/GameRuler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameRuler : MonoBehaviour
+{
+    [SerializeField] private Arrow arrow1;
+    [SerializeField] private Arrow arrow2;
+    [SerializeField] private Arrow arrow3;
+
+    public UnityEvent arrowCoincidedEvent;
+    public UnityEvent arrowNotCoincidedEvent;
+
+
+    public void checkMatch()
+    {
+        if (arrow1.collidedObject.GetComponent<Cell>().ID == arrow2.collidedObject.GetComponent<Cell>().ID &&
+            arrow2.collidedObject.GetComponent<Cell>().ID == arrow3.collidedObject.GetComponent<Cell>().ID)
+            arrowCoincidedEvent?.Invoke();
+        else arrowNotCoincidedEvent?.Invoke();
+    }
+}
